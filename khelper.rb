69dc2f7e5bf63dee84caa14db5f2c5d2ba09635a
Alexander <sasha.khyzhun@gmail.com>
2025-04-12 19:42:03 +0300

    Create khelper.rb

---
+++ khelper.rb
@@ -0,0 +1,11 @@
+class Khelper < Formula
+  desc "Personal CLI utility to speed up common tasks"
+  homepage "https://github.com/YOUR_USERNAME/khelper"
+  url "https://github.com/YOUR_USERNAME/khelper/releases/download/v1.0.0/khelper-macos.tar.gz"
+  sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  version "1.0.0"
+
+  def install
+    bin.install "khelper"
+  end
+end
