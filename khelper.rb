4aaa86f902e48d67b77ecd47010634fd77f11303
Alexander <sasha.khyzhun@gmail.com>
2025-05-04 21:50:26 +0300

    Update khelper.rb

--- khelper.rb
+++ khelper.rb
@@ -1,9 +1,9 @@
 class Khelper < Formula
   desc "Personal CLI utility to speed up common tasks!"
   homepage "https://github.com/khyzhun/khelper"
-  url "https://github.com/khyzhun/khelper/releases/download/v1.0.1/khelper-macos.tar.gz"
-  sha256 "f142027a7532498442f2d4bbcd4a8dc2075adc0d73dba3077c54c4816dd1aa53"
-  version "1.0.1"
+  url "https://github.com/khyzhun/khelper/releases/download/v1.0.2/khelper-macos.tar.gz"
+  sha256 "1234b092659b3eb11db59a7c600077bfa9c469d9db21ea877c3198b7be424169"
+  version "1.0.2"
 
   def install
     bin.install "khelper"
